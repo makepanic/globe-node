e145b7d18d376ca2df5b174dde8608a7fb1edf81
Christian <me@rndm.de>
2013-12-30 03:54:47 +0100

    search results, styles, more

--- routes/search.js
+++ routes/search.js
@@ -1,4 +1,5 @@
 var search = require('../lib/onionoo/search'),
+    formatter = require('../lib/util/formatter'),
     constants = require('../lib/static');
 
 module.exports = function(req, res){
@@ -8,7 +9,8 @@ module.exports = function(req, res){
             query: ''
         };
 
-    search(cfg.query, {
+    search({
+        query: cfg.query
     }).then(function(summaries){
         console.log('req.query', cfg);
 
@@ -28,7 +30,20 @@ module.exports = function(req, res){
             bridges: summaries.bridges
         };
 
+        data.model.relays.forEach(function(relay){
+            // formatter
+            relay.formattedBandwidth = formatter.bandwidth(relay.advertised_bandwidth);
+            relay.formattedCountryFlag = formatter.flaggify(relay.country);
+            relay.formattedUptime = formatter.uptimeShort(relay.last_restarted);
+            relay.formattedFlags = formatter.onionFlags(relay.flags);
+            if (relay.or_addresses.length) {
+                relay.formattedOrPort = formatter.port(relay.or_addresses[0]);
+            }
+            relay.formattedDirPort = formatter.port(relay.dir_address);
+        });
+
         res.render('search', data);
+
     }, function(err){
 
         data.alert = {
