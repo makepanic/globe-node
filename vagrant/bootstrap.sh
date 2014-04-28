e5668be0bf2cbf9124207a2a3dd83f2eceedc1ea
Christian <me@rndm.de>
2014-04-28 23:05:16 +0200

    fixed vagrant bootstrap

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -4,4 +4,9 @@
 echo "deb http://ftp.de.debian.org/debian/ wheezy-backports main" >> /etc/apt/sources.list
 
 apt-get update
-apt-get -y -t wheezy-backports install nodejs
+apt-get -y -t wheezy-backports install nodejs g++
+apt-get -y -t wheezy-backports install nodejs-legacy
+
+# TODO: find a way to handle dependencies without installing npm via install.sh
+# via https://github.com/joyent/node/wiki/Installing-Node.js-via-package-manager#backports
+curl https://www.npmjs.org/install.sh | sudo clean=no sh
